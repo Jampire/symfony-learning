1a68e4946e915b498bdc4b41a7bb343b480af69c
Dzianis (Den) Kotau <kotau@us.ibm.com>
2019-08-01 21:20:53 +0300

    Lesson 17

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Services\GiftsService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,12 +11,60 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends AbstractController
 {
     /**
-     * @Route("/default", name="default")
+     * @Route("/", name="default")
      */
     public function index(): Response
     {
+        $users = ['A', 'B', 'C', 'D'];
+
+//        $entityManager = $this->getDoctrine()->getManager();
+//        $user = (new User())->setName('Adam');
+//        $user2 = (new User())->setName('Robert');
+//        $user3 = (new User())->setName('John');
+//        $user4 = (new User())->setName('Susan');
+//        $entityManager->persist($user);
+//        $entityManager->persist($user2);
+//        $entityManager->persist($user3);
+//        $entityManager->persist($user4);
+//        $entityManager->flush();
+
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
+            'users' => $users,
         ]);
+    }
+
+    /**
+     * @Route("users", name="users_list")
+     * @param GiftsService $gifts
+     * @return Response
+     * @author Dzianis Den Kotau <kotau@us.ibm.com>
+     */
+    public function getUsers(GiftsService $gifts): Response
+    {
+        $users = $this->getDoctrine()->getRepository(User::class)->findAll();
+
+        return $this->render('default/index.html.twig', [
+            'controller_name' => 'DefaultController',
+            'users' => $users,
+            'random_gift' => $gifts->gifts,
+        ]);
+    }
+
+    /**
+     * @Route(
+     *     "/articles/{_locale}/{year}/{slug}/{category}",
+     *     defaults={"category": "computers"},
+     *     requirements={
+     *         "_locale": "by|en",
+     *         "category": "computers|rtv",
+     *         "year": "\d+"
+     *     }
+     * )
+     * @author Dzianis Den Kotau <kotau@us.ibm.com>
+     */
+    public function articles()
+    {
+
     }
 }
