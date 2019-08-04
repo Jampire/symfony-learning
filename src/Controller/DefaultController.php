09443acc9eee2ce98f3bcc4c9f2e9cdcc243d3a5
Dzianis (Den) Kotau <kotau@us.ibm.com>
2019-08-04 11:34:58 +0300

    Chapter 31

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,11 +5,18 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Services\GiftsService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class DefaultController extends AbstractController
 {
+    public function __construct($logger)
+    {
+
+    }
     /**
      * @Route("/", name="default")
      */
@@ -37,12 +44,15 @@ class DefaultController extends AbstractController
     /**
      * @Route("users", name="users_list")
      * @param GiftsService $gifts
+     * @param Request $request
      * @return Response
      * @author Dzianis Den Kotau <kotau@us.ibm.com>
      */
-    public function getUsers(GiftsService $gifts): Response
+    public function getUsers(GiftsService $gifts, Request $request): Response
     {
         $users = $this->getDoctrine()->getRepository(User::class)->findAll();
+
+        $this->addFlash('notice', 'AHAHAHA');
 
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
@@ -66,5 +76,70 @@ class DefaultController extends AbstractController
     public function articles()
     {
 
+    }
+
+    /**
+     * @Route("/cookie", name="my_cookie")
+     * @author Dzianis Den Kotau <kotau@us.ibm.com>
+     */
+    public function myCookie(): Response
+    {
+        $cookie = new Cookie('my_cookie', 'my_value', 3600);
+        $res = new Response();
+        $res->headers->setCookie($cookie);
+        $res->send();
+
+        return $res;
+    }
+
+    /**
+     * @Route("/generate-url/{param?}", name="generate_url")
+     * @author Dzianis Den Kotau <kotau@us.ibm.com>
+     */
+    public function myGenerateUrl()
+    {
+        exit($this->generateUrl('generate_url', [
+            'param' => 10,
+        ], UrlGeneratorInterface::ABSOLUTE_URL));
+    }
+
+    /**
+     * @Route("/download")
+     * @author Dzianis Den Kotau <kotau@us.ibm.com>
+     */
+    public function download()
+    {
+        $path = $this->getParameter('download_directory');
+
+        return $this->file($path . '/a.txt');
+    }
+
+    public function mostPopularPosts(int $number = 3)
+    {
+        $posts = ['post 1', 'post 2', 'posts 3', 'posts 4'];
+
+        return $this->render('default/most_popular_posts.html.twig', [
+            'posts' => $posts,
+        ]);
+    }
+
+    /**
+     * @Route("/save/{name}", name="crete_user")
+     * @param string $name
+     *
+     * @author Dzianis Den Kotau <kotau@us.ibm.com>
+     * @return Response
+     */
+    public function saveUser(string $name): Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $user = new User();
+        $user->setName($name);
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        return $this->render('default/create_user.html.twig', [
+            'user_id' => $user->getId(),
+        ]);
     }
 }
