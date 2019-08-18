246e4572516c1feeb91a5e0523b8c82f0cdee52a
Dzianis (Den) Kotau <kotau@us.ibm.com>
2019-08-19 02:25:33 +0300

    Chapter 67

--- src/EventSubscriber/VideoCreatedSubscriber.php
+++ src/EventSubscriber/VideoCreatedSubscriber.php
@@ -4,18 +4,39 @@ namespace App\EventSubscriber;
 
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use App\Events\VideoCreatedEventInterface;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\KernelEvents;
 
 class VideoCreatedSubscriber implements EventSubscriberInterface
 {
-    public function onVideoCreatedEvent(VideoCreatedEventInterface $event)
+    public function onVideoCreatedEvent(VideoCreatedEventInterface $event): void
     {
         dump($event->getVideo()->title);
+    }
+
+    public function onKernelResponse1(ResponseEvent $event): void
+    {
+//        $response = new Response('jampire');
+//        $event->setResponse($response);
+        dump('onKernelResponse1');
+    }
+
+    public function onKernelResponse2(ResponseEvent $event): void
+    {
+//        $response = new Response('jampire');
+//        $event->setResponse($response);
+        dump('onKernelResponse2');
     }
 
     public static function getSubscribedEvents()
     {
         return [
             'video.created.event' => 'onVideoCreatedEvent',
+            KernelEvents::RESPONSE => [
+                ['onKernelResponse1', 2],
+                ['onKernelResponse2', 1],
+            ],
         ];
     }
 }
