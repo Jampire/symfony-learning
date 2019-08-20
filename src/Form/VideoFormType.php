ab0244e08ede3c3f5d56c93e0a57a8a9f9f0e0be
Dzianis (Den) Kotau <kotau@us.ibm.com>
2019-08-20 20:56:01 +0300

    Chapter 75

--- src/Form/VideoFormType.php
+++ src/Form/VideoFormType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Video;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -21,6 +22,10 @@ class VideoFormType extends AbstractType
                 'label' => 'Set video title',
                 //'data' => 'Example data',
                 'required' => false,
+            ])
+            ->add('agreeTerms', CheckboxType::class, [
+                'label' => 'Agree ?',
+                'mapped' => false,
             ])
             ->add('save', SubmitType::class, [
                 'label' => 'Add video'
